c177364ba7aeb3ffdcb84080eb3150f99eee319f
Ammarhr <hariry49@gmail.com>
2022-10-30 14:23:24 +0300

    last changes

--- src/Postman.jsx
+++ src/Postman.jsx
@@ -3,6 +3,8 @@ import { createElement } from "react";
 import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/Postman.css";
 
-export function Postman({ urltext, textName, imgKey }) {
-    return <HelloWorldSample urltext={urltext} textName={textName} imgKey={imgKey}  />;
+export function Postman({ urltext, textName, imgKey, widgetList, items }) {
+    return (
+        <HelloWorldSample urltext={urltext} textName={textName} imgKey={imgKey} widgetList={widgetList} items={items} />
+    );
 }
--- src/components/HelloWorldSample.jsx
+++ src/components/HelloWorldSample.jsx
@@ -3,15 +3,14 @@ import { Card, Col, Row } from "react-bootstrap";
 import { createElement } from "react";
 import axios from "axios";
 
-export function HelloWorldSample({ urltext, textName, imgKey }) {
+export function HelloWorldSample({ urltext, textName, imgKey, widgetList, items }) {
     const [data, setData] = useState([]);
     const options = {
         method: "get",
-        url: urltext,
+        url: urltext
     };
     useEffect(() => {
         axios(options).then(response => {
-            console.log("data", response);
             setData(response.data);
         });
     }, []);
@@ -19,32 +18,12 @@ export function HelloWorldSample({ urltext, textName, imgKey }) {
         <div className="widget-hello-world">
             <h4 style={{ color: "purple" }}> Url: {urltext}</h4>
             <Row xs={1} md={4} className="g-4">
-                {data
-                    ? data.map(element => {
-                          element["name"] = element[textName];
-                          delete element[textName];
-
-                          element["img"] = element[imgKey];
-                          delete element[imgKey];
-
-                          return (
-                              <>
-                                  <Col>
-                                      <Card>
-                                          <Card.Img variant="top" src={element.img} alt={element.name} />
-                                          <Card.Body>
-                                              <Card.Title> {element.name}</Card.Title>
-                                          </Card.Body>
-                                      </Card>
-                                  </Col>
-                                  {/* {Object.values(element).map(val => {
-                                  if (typeof val == "string")
-                                      return val.match("[a-z-_0-9/:.]*.(jpg|jpeg|png|gif)") ? <img src={val} /> : "";
-                              })} */}
-                              </>
-                          );
-                      })
-                    : "Nothing to show"}
+                <div>
+                    {items.items &&
+                        items.items.map(item => {
+                            return <h1>{widgetList.get(item)}</h1>
+                        })}
+                </div>
             </Row>
         </div>
     );
